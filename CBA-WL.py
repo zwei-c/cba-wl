cca72913bbdff59cf60ea61467aecfa2415fb191
wei <yie099063@gmail.com>
2024-06-23 07:20:18 +0800

    env(poetry) 修改python版本

--- CBA-WL.py
+++ CBA-WL.py
@@ -1,41 +1,41 @@
-from ucimlrepo import fetch_ucirepo
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from mlxtend.preprocessing import TransactionEncoder
+import os
+
 import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from ucimlrepo import fetch_ucirepo
+
+DATASETS = {
+    19: 'Car Evaluation',
+}
 
 
-class CBA_WL:
+class DataPreprocessing:
     def __init__(self, dataset_id):
         self.dataset_id = dataset_id
-        self.X_train, self.X_test, self.y_train, self.y_test, self.train, self.test = self.loadData()
+        self.data, self.features, self.target = self.loadData()
 
     def loadData(self):
-        dataset = fetch_ucirepo(id=self.dataset_id)
-        features = dataset.data.features
-        target = dataset.data.targets
 
-    def randomForestFeatureImportances(self):
-        print('Random Forest Feature Importances')
+        file_dir_path = "./data"
 
-    def oneHotEncoding(self, df):
-        # 將特徵轉換為所需格式，排除 'target' 列
-        features_list = []
-        for index, row in df.iterrows():
-            features = []
-            for col, value in row.items():
-                features.append(f"{col}: {value}")
-            features_list.append(features)
-        # 使用 TransactionEncoder 進行編碼
-        te = TransactionEncoder()
-        te_ary = te.fit(features_list).transform(features_list)
-        # 將編碼後的數據轉換為 DataFrame
-        te_df = pd.DataFrame(te_ary, columns=te.columns_)
+        if not os.path.exists(file_dir_path):
+            os.makedirs(file_dir_path)
+
+        data_name = DATASETS[self.dataset_id]
+        if not os.path.exists(f"{file_dir_path}/{data_name}.csv"):
+            url = f"https://archive.ics.uci.edu/static/public/{self.dataset_id}/data.csv"
+            data = pd.read_csv(url)
+            data.to_csv(f"{file_dir_path}/{data_name}.csv", index=False)
+        else:
+            data = pd.read_csv(f"{file_dir_path}/{data_name}.csv")
+
+        # features = dataset.data.features
+        # target = dataset.data.targets
+        # return dataset.data, features, target
 
 
 if __name__ == '__main__':
-    dataset_dict = {
-        'Car Evaluation': 19
-    }
-    cba = CBA_WL(19)
-    # cba.randomForestFeatureImportances()
+    dataset_id = 19
+    data = DataPreprocessing(dataset_id)
