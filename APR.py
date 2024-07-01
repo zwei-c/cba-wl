46cc1f7c2dfce71bdd42a1fb69dd890ff24a202b
wei <yie099063@gmail.com>
2024-07-01 22:09:21 +0800

    fix

--- APR.py
+++ APR.py
@@ -1,4 +1,4 @@
-class CBA:
+class APR:
     def __init__(self, data, min_sup, min_conf):
         self.data = data
         self.min_sup = min_sup
@@ -27,8 +27,11 @@ class CBA:
                                                   'support': support, 'confidence': confidence})
         return frequent_itemsets
 
-    def calculate(self, itemset):
-        data = self.data.copy()
+    def calculate(self, itemset, data=None):
+        if data is None:
+            data = self.data.copy()
+        else:
+            data = data.copy()
         condition = itemset['condition']
         class_value = itemset['class']
 
@@ -80,50 +83,45 @@ class CBA:
         while True:
             frequent_itemset = self.generate_frequents(frequent_itemset, k)
 
-            if len(frequent_itemset) == 0:
+            if len(frequent_itemset) == 0 or k >= 4:
                 break
             else:
                 k += 1
                 frequent_itemsets.extend(frequent_itemset)
+
         return frequent_itemsets
 
     def prune(self, sorted_ruleitemset):
         data = self.data.copy()
         data_ = data.copy()
-        data_y = data['class']
         rules = []
+        default_class = data_['class'].value_counts().idxmax()
 
-        for ruleitem in sorted_ruleitemset:
-            # 找到符合當前規則的索引
-            cover_index = [index for index, row in data_.iterrows() if self.cover_for_prune(ruleitem, row)]
+        while not data_.empty:
+            selected_rule = sorted_ruleitemset[0]
+            cover_index = [index for index, row in data_.iterrows() if self.cover_for_prune(selected_rule, row)]
 
             if cover_index:
-                # 刪除符合當前規則的資料行
                 data_ = data_.drop(cover_index, axis=0)
-                rules.append(ruleitem)
-
-                # 確定預設類別
+                rules.append(selected_rule)
                 default_class = data_['class'].value_counts().idxmax()
-                y = self.predict(data, rules, default_class)
 
-                # 計算錯誤率
-                error = sum(data_y[i] != y[i] for i in range(len(data_y)))
-
-                ruleitem['error'] = error
-                ruleitem['default_class'] = default_class
-
-                rules[-1] = ruleitem
-
-            if data_.empty:
+                sorted_ruleitemset_ = sorted_ruleitemset[1:]
+                ruleitemset = []
+                for ruleitem in sorted_ruleitemset_:
+                    sup, conf = self.calculate(ruleitem, data_)
+                    if sup > self.min_sup and conf > self.min_conf:
+                        ruleitem['support'] = sup
+                        ruleitem['confidence'] = conf
+                        ruleitemset.append(ruleitem)
+                if len(ruleitemset) != 0:
+                    sorted_ruleitemset = sorted(ruleitemset, key=lambda x: (
+                        x['confidence'], x['support'], len(x['condition'])), reverse=True)
+                else:
+                    break
+            else:
                 break
 
-        # 找到最小錯誤率的規則
-        min_error_rule = min(rules, key=lambda x: x['error'])
-        min_error_index = rules.index(min_error_rule)
-        default_class = min_error_rule['default_class']
-
-        # 返回修剪後的規則和預設類別
-        rules = rules[:min_error_index + 1]
         return rules, default_class
 
     def cover_for_prune(self, ruleitem, instance):
@@ -138,12 +136,19 @@ class CBA:
 
     def predict(self, data, rules, default_class=None):
         predict_y = []
+        class_group = {}
 
         # 預測每一行的類別
         for _, row in data.iterrows():
-            # 使用 next 和生成器表達式找到符合條件的第一個規則
-            predicted_class = next((rule['class']
-                                   for rule in rules if self.cover_for_predict(rule, row)), default_class)
-            predict_y.append(predicted_class)
+            for rule in rules:
+                if self.cover_for_predict(rule, row):
+                    if rule['class'] in class_group:
+                        class_group[rule['class']] += 1
+                    else:
+                        class_group[rule['class']] = 1
+            if len(class_group) == 0:
+                predict_y.append(default_class)
+            else:
+                predict_y.append(max(class_group, key=class_group.get))
 
         return predict_y
--- RWCBA.py
+++ RWCBA.py
@@ -1,8 +1,10 @@
 class RWCBA:
-    def __init__(self, data):
+    def __init__(self, data, importance=[]):
         self.data = data
+        self.importance = importance
 
-    def genFrequentOne(self, data, min_sup=0.1, min_conf=0.5):
+    def genFrequentOne(self, min_sup=0.1, min_conf=0.5):
+        data = self.data.copy()
         frequent_itemsets = []
         features = data.columns[:-1]
         class_values = data['class'].unique()
@@ -11,19 +13,35 @@ class RWCBA:
                 feature_values = data[feature].unique()
                 for value in feature_values:
                     count = data[(data[feature] == value) & (data['class'] == class_value)].shape[0]
+
+                    if count == 0:
+                        continue
+
                     support = count / data.shape[0]
+                    support *= self.importance[feature]
                     confidence = count / data[data[feature] == value].shape[0]
                     if support >= min_sup and confidence >= min_conf:
                         condition = [{feature: value}]
+                        hm = 2 * support * confidence / (support + confidence)
                         frequent_itemsets.append({'condition': condition, 'class': class_value,
-                                                  'support': support, 'confidence': confidence})
+                                                  'support': support, 'confidence': confidence, 'hm': hm})
         return frequent_itemsets
 
-    def calculate(self, data, itemset):
+    def calculateConditionWeight(self, condition):
+        importance = self.importance
+        condition_len = len(condition)
+        weight = 0
+        for cond in condition:
+            weight += importance[list(cond.keys())[0]]
+        return weight / condition_len
+
+    def calculate(self, itemset):
+        data = self.data.copy()
         condition = itemset['condition']
         class_value = itemset['class']
 
         condition_condition = data[list(condition[0].keys())[0]] == list(condition[0].values())[0]
+
         for cond in condition[1:]:
             condition_condition &= (data[list(cond.keys())[0]] == list(cond.values())[0])
 
@@ -35,12 +53,12 @@ class RWCBA:
             return 0, 0
 
         support = itemset_support_count / data.shape[0]
+        weight_support = support * self.calculateConditionWeight(condition)
         confidence = itemset_support_count / condition_support_count
-        return support, confidence
+        return weight_support, confidence
 
     def generate_frequents(self, freq_itemsets, k, min_sup=0.01, min_conf=0.5):
         frequents = []
-
         for i in range(len(freq_itemsets)):
             for j in range(i + 1, len(freq_itemsets)):
                 if freq_itemsets[i]['class'] == freq_itemsets[j]['class']:
@@ -54,20 +72,22 @@ class RWCBA:
                             for cond in new_condition:
                                 condition.append({cond[0]: cond[1]})
                             candidate = {'condition': condition, 'class': freq_itemsets[i]['class']}
-                            sup, conf = self.calculate(self.data, candidate)
-                            if sup > min_sup and conf > min_conf and conf > freq_itemsets[i]['confidence'] and conf > freq_itemsets[j]['confidence']:
-                                candidate['support'] = sup
+                            weight_support, conf = self.calculate(candidate)
+                            if weight_support > min_sup and conf > min_conf and conf > freq_itemsets[i]['confidence'] and conf > freq_itemsets[j]['confidence']:
+                                candidate['support'] = weight_support
                                 candidate['confidence'] = conf
+                                candidate['hm'] = 2 * candidate['support'] * candidate['confidence'] / \
+                                    (candidate['support'] + candidate['confidence'])
                                 frequents.append(candidate)
         return frequents
 
-    def apriori(self, data, min_sup=0.01, min_conf=0.5):
+    def apriori(self, min_sup=0.01, min_conf=0.5):
         frequent_itemsets = []
-        frequent_itemset = self.genFrequentOne(data, min_sup, min_conf)
+        frequent_itemset = self.genFrequentOne(min_sup, min_conf)
         frequent_itemsets.extend(frequent_itemset)
         k = 2
         while True:
-            frequent_itemset = self.generate_frequents(frequent_itemset, k)
+            frequent_itemset = self.generate_frequents(frequent_itemset, k, min_sup, min_conf)
 
             if len(frequent_itemset) == 0:
                 break
@@ -76,11 +96,13 @@ class RWCBA:
                 frequent_itemsets.extend(frequent_itemset)
         return frequent_itemsets
 
-    def prune(self, data, sorted_ruleitemset):
+    def prune(self, sorted_ruleitemset):
+        data = self.data.copy()
         data_ = data.copy()
         data_y = data['class']
         rules = []
-
+        strong_rules = []
+        spare_rules = []
         for ruleitem in sorted_ruleitemset:
             # 找到符合當前規則的索引
             cover_index = [index for index, row in data_.iterrows() if self.cover_for_prune(ruleitem, row)]
@@ -92,7 +114,7 @@ class RWCBA:
 
                 # 確定預設類別
                 default_class = data_['class'].value_counts().idxmax()
-                y = self.predict(data, rules, default_class)
+                y = self.predict_for_prune(rules, default_class)
 
                 # 計算錯誤率
                 error = sum(data_y[i] != y[i] for i in range(len(data_y)))
@@ -101,6 +123,8 @@ class RWCBA:
                 ruleitem['default_class'] = default_class
 
                 rules[-1] = ruleitem
+            else:
+                spare_rules.append(ruleitem)
 
             if data_.empty:
                 break
@@ -111,8 +135,10 @@ class RWCBA:
         default_class = min_error_rule['default_class']
 
         # 返回修剪後的規則和預設類別
-        rules = rules[:min_error_index + 1]
-        return rules, default_class
+        strong_rules = rules[:min_error_index + 1]
+        spare_rules.extend(rules[min_error_index + 1:])
+        spare_rules.sort(key=lambda x: x['hm'], reverse=True)
+        return strong_rules, spare_rules, default_class
 
     def cover_for_prune(self, ruleitem, instance):
         condition = ruleitem['condition']
@@ -124,14 +150,44 @@ class RWCBA:
         condition = ruleitem['condition']
         return all(instance[list(cond.keys())[0]] == list(cond.values())[0] for cond in condition)
 
-    def predict(self, data, rules, default_class=None):
+    def predict_for_prune(self, rules,  default_class=None):
+        data = self.data.copy()
         predict_y = []
-
         # 預測每一行的類別
         for _, row in data.iterrows():
             # 使用 next 和生成器表達式找到符合條件的第一個規則
             predicted_class = next((rule['class']
-                                   for rule in rules if self.cover_for_predict(rule, row)), default_class)
+                                    for rule in rules if self.cover_for_predict(rule, row)), default_class)
             predict_y.append(predicted_class)
+
+        return predict_y
+
+    def predict(self, data, strong_rules, spare_rules, default_class=None):
+        predict_y = []
+        # 預測每一行的類別
+        for _, row in data.iterrows():
+            class_group = {}
+            for rule in strong_rules:
+                if self.cover_for_predict(rule, row):
+                    class_value = rule['class']
+                    if class_value in class_group.keys():
+                        class_group[class_value].append(rule['hm'])
+                    else:
+                        class_group[class_value] = [rule['hm']]
+
+            if not class_group:
+                for rule in spare_rules:
+                    if self.cover_for_predict(rule, row):
+                        class_value = rule['class']
+                        if class_value in class_group.keys():
+                            class_group[class_value].append(rule['hm'])
+                        else:
+                            class_group[class_value] = [rule['hm']]
+
+            if class_group:
+                mean_hm = {k: sum(v) / len(v) for k, v in class_group.items()}
+                predict_y.append(max(mean_hm, key=mean_hm.get))
+            else:
+                predict_y.append(default_class)
 
         return predict_y
